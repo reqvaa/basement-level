0141e58d4b3c5133a862b855866bd7a2e72f404f
Alexei E <rr3qva@gmail.com>
2017-03-16 17:54:23 +0800

    Add files via upload

---
+++ phone_mk1.java
@@ -0,0 +1,29 @@
+package education;
+
+
+public class Phone_mk1(String S) {
+	char[] phone_ch = new S.toCharArray();
+	int number_pask = 3;
+	//char[] phone_number = new phone_number();
+	ArrayList<phone_ch> phone_number() = new Arraylist<phone_ch>;
+		for (char ch : phone_ch) {
+			if (ch!=' '|| ch!='-'){
+				phone_number.Add(ch);
+				number_pack--;
+				if (number_pack = 0){
+					number_pack = 3;
+					phone_number.Add('-');
+				}
+				
+				
+			}
+		}
+System.out.println(+phone_number);
+;
+}
+
+public class Phone_number();
+	public static void main(String[] args){
+		phone_mk1 first = new phone_mk1('342342-2-234234 2-3 234 23 ');
+		System.out.println(+first);
+	}

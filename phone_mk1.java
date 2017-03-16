147b695517a8dfed64a6623e4298d5897eab5bec
Alexei E <rr3qva@gmail.com>
2017-03-16 20:56:28 +0800

    Update phone_mk1.java
    first working code for codility test
    (3rd iteration of changes)

--- phone_mk1.java
+++ phone_mk1.java
@@ -1,29 +1,60 @@
 package education;
+import java.util.*;
+
+public class Phone_mk1{
+	public static void main (String[] args){
+		//public void class phone_n(){
+			String S = "123123-101231023 12 122 3   123121-";
+			int length = S.length();
+		    char s_ch1 = ' ';
+		    char s_ch2 = '-';
+		    ArrayList<String> phone_number = new ArrayList<String>();
+		    int block_of_3 = 3;
+		    for (int i = 0; i < length; ++i) {
+		        if (S.charAt(i) != s_ch1 && S.charAt(i) != s_ch2 ) {
+		        	String a = Character.toString(S.charAt(i));
+		        	phone_number.add(a);
+		        	block_of_3--;
+		        	if (block_of_3 == 0){
+		        		phone_number.add("-");
+		        		block_of_3 = 3;
+		        	}
+		        	else{
+		        		
+		        	}
+		        }
+		         System.out.println(phone_number);
+		}
+
+		
+	}
+	}
+//}
 
 
-public class Phone_mk1(String S) {
+
+
+/*
+
+public class Phone_number(String S){
+	String S;
 	char[] phone_ch = new S.toCharArray();
 	int number_pask = 3;
 	//char[] phone_number = new phone_number();
-	ArrayList<phone_ch> phone_number() = new Arraylist<phone_ch>;
+	Char[] phone_number = new char[];
 		for (char ch : phone_ch) {
 			if (ch!=' '|| ch!='-'){
 				phone_number.Add(ch);
 				number_pack--;
-				if (number_pack = 0){
+				if (number_pack == 0){
 					number_pack = 3;
 					phone_number.Add('-');
 				}
-				
-				
+				                                                                    
 			}
+			
 		}
-System.out.println(+phone_number);
+		System.out.println(char[phone_number]);
 ;
 }
-
-public class Phone_number();
-	public static void main(String[] args){
-		phone_mk1 first = new phone_mk1('342342-2-234234 2-3 234 23 ');
-		System.out.println(+first);
-	}
+*/
